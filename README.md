a82437209215f46fd272dc06438d010dde6c7f28
abhi <abhi>
2023-11-28 16:58:09 +0530

    deleted

--- README.md
+++
@@ -1,2 +0,0 @@
-
-# docker run --name test-graph-postgres -p 5002:5432 --env-file .env -d postgres
--- graph_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import HiView
-
-urlpatterns = [
-    path('hi/', HiView.as_view(), name='hi'),
-]
